c6b514e1c8349c5199a7d04c5d0c0f031eebebdd
taep96 <64481039+taep96@users.noreply.github.com>
2023-02-14 22:58:44 +0100

    switch from head.tsx to metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,40 @@ import "@/app/global.css";
 import Archbtw from "@/components/archbtw";
 import { JetBrains_Mono } from "@next/font/google";
 
+const title = "taep96's epic website";
+const description = "Hey there! I'm taep96!";
+
+const { href: canonical } = new URL(`https://${process.env.VERCEL_URL!}/`);
+
+export const metadata = {
+  title: {
+    default: title,
+    template: `%s | ${title}`,
+  },
+  description: description,
+  themeColor: "#202231",
+  alternates: { canonical },
+  openGraph: {
+    title,
+    description,
+    url: canonical,
+    images: [],
+    type: "website",
+  },
+  twitter: {
+    card: "summary",
+    title,
+    description,
+    siteId: "1541447528595554310",
+    creatorId: "1541447528595554310",
+    images: canonical + "apple-touch-icon.png",
+  },
+  icons: {
+    icon: ["/favicon-16x16.png", "/favicon-32x32.png"],
+    apple: "/apple-touch-icon.png",
+  },
+};
+
 const fontMono = JetBrains_Mono({
   subsets: ["latin"],
   variable: "--font-jetbrains-mono",
