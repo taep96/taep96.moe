e38f6f223e8122557df0bd33ef53606713463cdb
taep96 <64481039+taep96@users.noreply.github.com>
2023-02-15 21:56:21 +0100

    404 page

---
+++ src/app/not-found/page.tsx
@@ -0,0 +1,25 @@
+/** Next.js currently doesn't have support
+ *  for unmatched route pages in `app` directory,
+ *  so this page will only show on request to
+ * `/not-found` and won't return HTTP 404.
+ */
+
+import Kaomoji from "./kaomoji";
+
+export const metadata = {
+  title: `Page not found`,
+};
+
+export default function NotFoundPage() {
+  return (
+    <>
+      <h1 className="after:animate-cursor after:bg-fennel after:text-fennel after:content-['-']">
+        404 <Kaomoji />
+      </h1>
+      <p>
+        Couldn&apos;t find the page you were looking for -{" "}
+        <a href="/">go home?</a>
+      </p>
+    </>
+  );
+}
