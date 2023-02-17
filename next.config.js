37b4df6272312d59b5ad75cefa22831ffb91b9ae
taep96 <64481039+taep96@users.noreply.github.com>
2023-02-17 22:47:37 +0100

    allow vercel web vitals, attempt #6

--- next.config.js
+++ next.config.js
@@ -23,6 +23,7 @@ const ContentSecurityPolicy = `
   script-src 'self' 'unsafe-eval' 'unsafe-inline' vitals.vercel-insights.com;
   style-src 'self' 'unsafe-inline';
   img-src * blob: data:;
+  connect-src *;
   font-src 'self';
 `;
 
