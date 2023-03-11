0854d5a1044706e94a3bd0dc3cf92c2f8c19ab33
taep96 <64481039+taep96@users.noreply.github.com>
2023-03-11 16:46:47 +0100

    fix cursor causing layout shift

--- app/cursor.tsx
+++ app/cursor.tsx
@@ -6,7 +6,7 @@ export default function BackgroundCursor() {
   return (
     <>
       <div className="absolute -z-40 h-screen w-screen backdrop-blur-3xl" />
-      <EfficientCursor className="-z-50">
+      <EfficientCursor className="absolute -z-50">
         <div className="aspect-square h-[50vh] animate-rotate rounded-full bg-gradient-to-r from-coral to-salmon opacity-30" />
       </EfficientCursor>
     </>
